c80a72554efc66b72be41a23d6d5fc6889280d2e
abandaru403 <abandaru403@aol.com>
2025-06-22 16:14:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-echo "<h1>Welcome to My PHP App on Azure!</h1>";
-echo "<p>This is the homepage.</p>";
-echo "<a href='about.php'>Go to About Page</a>";
+// Set default timezone
+date_default_timezone_set('UTC');
+
+// Simple dynamic content
+$time = date("Y-m-d H:i:s");
+$ip = $_SERVER['REMOTE_ADDR'];
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to My PHP App</title>
+</head>
+<body>
+    <h1>🚀 PHP App Deployed on Azure!</h1>
+    <p>Current server time is: <strong><?php echo $time; ?></strong></p>
+    <p>Your IP address is: <strong><?php echo $ip; ?></strong></p>
+</body>
+</html>
